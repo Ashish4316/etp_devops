6d110714f7109064f33fadf71e1d878b06784289
Ashish <yadavashish72408@gmail.com>
2026-06-08 13:01:07 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "database",      
+  user: "root",
+  password: "rootpassword",
+  database: "ecommerce_db"
+});
+
+connection.connect((err) => {
+  if(err){
+    console.error("Connection failed:", err);
+    return;
+  }
+  console.log("Connected to MySQL!");
+});
+
